1ce6a3e198ade1d0329551ad9e63571a61cfa5a1
MXMK <mxmk1799@yandex.ru>
2019-12-06 22:51:23 +0300

    header fix

--- Controllers/ApiController.php
+++ Controllers/ApiController.php
@@ -14,9 +14,14 @@
                         $this->response($users, 'json');
                     }
                 } else {
+                    header("Access-Control-Allow-Origin: *");
+                    header("Access-Control-Allow-Methods: *");
+                    header("HTTP/1.0 400 Bad Request");
                     echo json_encode(["error" => "Incorrect key"]);
                 }
             } else {
+                header("Access-Control-Allow-Origin: *");
+                header("Access-Control-Allow-Methods: *");
                 header("HTTP/1.0 400 Bad Request");
                 echo json_encode(["error" => "Please, specify key"]);
             }
