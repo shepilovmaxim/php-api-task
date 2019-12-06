46051a5f38e23867b2c07e8b919198c64ce3be79
MXMK <mxmk1799@yandex.ru>
2019-12-06 22:46:42 +0300

    2

--- Controllers/ApiController.php
+++ Controllers/ApiController.php
@@ -32,6 +32,8 @@
                         }
                     }
                 }
+                header("Access-Control-Allow-Origin: *");
+                header("Access-Control-Allow-Methods: *");
                 header('Content-Type:application/json');
                 echo json_encode($users);
             } else if ($type == 'xml') {
@@ -47,6 +49,8 @@
                         $userrow->addChild($property, $value);
                     }
                 }
+                header("Access-Control-Allow-Origin: *");
+                header("Access-Control-Allow-Methods: *");
                 header('Content-type: text/xml');
                 echo $xml->asXML();
             }
